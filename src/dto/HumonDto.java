344e10e30877c7f6c72c3103c1568db3ee41e5de
ha-rang2 <rhu2222@gmail.com>
2025-11-27 16:15:56 +0900

    TestBranch

--- src/dto/HumonDto.java
+++ src/dto/HumonDto.java
@@ -3,4 +3,25 @@ package dto;
 public class HumonDto {
 	private String name;
 	private int age;
+
+	public HumonDto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
